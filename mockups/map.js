488b8ecab0b1b235b97e1cbbbe2a861306fc8cb4
Megha <meghashrestha2@gmail.com>
2015-08-24 16:42:52 +0545

    first changes for landing page

---
+++ mockups/map.js
@@ -0,0 +1,5 @@
+var map = L.map ('map').setView([27.4,85.12],13);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
