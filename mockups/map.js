a33c859132cf34c8c8e7736e872c43fb6cf18547
Megha <meghashrestha2@gmail.com>
2015-08-24 17:04:11 +0545

    changed the zoom level of map

--- mockups/map.js
+++ mockups/map.js
@@ -1,4 +1,4 @@
-var map = L.map ('map').setView([27.4,85.12],13);
+var map = L.map ('map').setView([28.1,85.12],7);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
