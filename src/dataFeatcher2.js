6b28a7720e859f1a6080c19ec0e5f5647d14234c
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-09-14 11:44:07 +0545

    Update dataFeatcher2.js
    more async

--- src/dataFeatcher2.js
+++ src/dataFeatcher2.js
@@ -1,7 +1,8 @@
-var loadingDataDeffered = $.Deferred();
+var loadingDataDeffered = []; = $.Deferred();
+for(var i = 0; i < 13; i++)
+	loadingDataDeffered[0]= $.Deferred();
+	
 function fetchData(query_type, fromYear, toYear, bbox) {
-	
-	
 	fromYear = (typeof fromYear === 'undefined') ? '2010' : fromYear;
 	toYear = (typeof toYear === 'undefined') ? '2010' : toYear;
 	bbox = (typeof bbox === 'undefined') ? '' : bbox;
@@ -10,62 +11,62 @@ function fetchData(query_type, fromYear, toYear, bbox) {
 		switch(query_type) {
 			case 'roads':
 				//return nepalStats.all[0];
-				loadingDataDeffered.resolve(nepalStats[0]);
+				loadingDataDeffered[0].resolve(nepalStats[0]);
 				break;
 			case 'waterways':
 				//return nepalStats.all[1];
-				loadingDataDeffered.resolve(nepalStats[1]);
+				loadingDataDeffered[1].resolve(nepalStats[1]);
 				break;
 			case 'edu_institute':
 				//return nepalStats.all[2];
-				loadingDataDeffered.resolve(nepalStats[2]);
+				loadingDataDeffered[2].resolve(nepalStats[2]);
 				break;
 			case 'buildings':
 				//return nepalStats.all[3];
-				loadingDataDeffered.resolve(nepalStats[3]);
+				loadingDataDeffered[3].resolve(nepalStats[3]);
 				break;
 			case 'medical':
 				//return nepalStats.all[4];
-				loadingDataDeffered.resolve(nepalStats[4]);
+				loadingDataDeffered[4].resolve(nepalStats[4]);
 				break;
 			case 'financial_institute':
 				//return nepalStats.all[5];
-				loadingDataDeffered.resolve(nepalStats[5]);
+				loadingDataDeffered[5].resolve(nepalStats[5]);
 				break;
 			case 'gov_offices':
 				//return nepalStats.all[6];
-				loadingDataDeffered.resolve(nepalStats[6]);
+				loadingDataDeffered[6].resolve(nepalStats[6]);
 				break;
 			case 'historic_sites':
 				//return nepalStats.all[7];
-				loadingDataDeffered.resolve(nepalStats[7]);
+				loadingDataDeffered[7].resolve(nepalStats[7]);
 				break;
 			case 'natural_heritage':
 				//return nepalStats.all[8];
-				loadingDataDeffered.resolve(nepalStats[8]);
+				loadingDataDeffered[8].resolve(nepalStats[8]);
 				break;
 			case 'tourist_interest':
 				//return nepalStats.all[9];
-				loadingDataDeffered.resolve(nepalStats[9]);
+				loadingDataDeffered[9].resolve(nepalStats[9]);
 				break;
 			case 'settlement':
 				//return nepalStats.all[10];
-				loadingDataDeffered.resolve(nepalStats[10]);
+				loadingDataDeffered[10].resolve(nepalStats[10]);
 				break;
 			case 'e_i_y':
 				//return nepalStats.all[11];
-				loadingDataDeffered.resolve(nepalStats[11]);
+				loadingDataDeffered[11].resolve(nepalStats[11]);
 				break;
 			case 'users':
 				//return nepalStats.all[12];
-				loadingDataDeffered.resolve(nepalStats[12]);
+				loadingDataDeffered[12].resolve(nepalStats[12]);
 				break;
 		}
 	} else {
 	
 		setLoader();
 		
-		var apiURL = 'http://45.55.246.231:3000/api/' + query_type + '?from=';
+		var apiURL = 'http://45.55.246.231:8000/api/' + query_type + '?from=';
 		apiURL += fromYear + '&to=';
 		apiURL += toYear + '&bbox=' + bbox;
 		console.log(apiURL);
@@ -75,10 +76,65 @@ function fetchData(query_type, fromYear, toYear, bbox) {
 			success: function(result) {
 				hideLoader();
 				data = result;
-				loadingDataDeffered.resolve(data);
+				console.log(data);
+				switch(query_type) {
+			case 'roads':
+				//return nepalStats.all[0];
+				loadingDataDeffered[0].resolve(nepalStats[0]);
+				break;
+			case 'waterways':
+				//return nepalStats.all[1];
+				loadingDataDeffered[1].resolve(nepalStats[1]);
+				break;
+			case 'edu_institute':
+				//return nepalStats.all[2];
+				loadingDataDeffered[2].resolve(nepalStats[2]);
+				break;
+			case 'buildings':
+				//return nepalStats.all[3];
+				loadingDataDeffered[3].resolve(nepalStats[3]);
+				break;
+			case 'medical':
+				//return nepalStats.all[4];
+				loadingDataDeffered[4].resolve(nepalStats[4]);
+				break;
+			case 'financial_institute':
+				//return nepalStats.all[5];
+				loadingDataDeffered[5].resolve(nepalStats[5]);
+				break;
+			case 'gov_offices':
+				//return nepalStats.all[6];
+				loadingDataDeffered[6].resolve(nepalStats[6]);
+				break;
+			case 'historic_sites':
+				//return nepalStats.all[7];
+				loadingDataDeffered[7].resolve(nepalStats[7]);
+				break;
+			case 'natural_heritage':
+				//return nepalStats.all[8];
+				loadingDataDeffered[8].resolve(nepalStats[8]);
+				break;
+			case 'tourist_interest':
+				//return nepalStats.all[9];
+				loadingDataDeffered[9].resolve(nepalStats[9]);
+				break;
+			case 'settlement':
+				//return nepalStats.all[10];
+				loadingDataDeffered[10].resolve(nepalStats[10]);
+				break;
+			case 'e_i_y':
+				//return nepalStats.all[11];
+				loadingDataDeffered[11].resolve(nepalStats[11]);
+				break;
+			case 'users':
+				//return nepalStats.all[12];
+				loadingDataDeffered[12].resolve(nepalStats[12]);
+				break;
+		}
+				//loadingDataDeffered.resolve(data);
 				hideLoader();
 			},
-			async: false
+			async: true
 		});
 	}
 }
