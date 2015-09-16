0d18b01429d686392677acb139731ffd9d534e69
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-09-16 18:36:35 +0545

    Update dataFeatcher2.js
    temp commmit

--- src/dataFeatcher2.js
+++ src/dataFeatcher2.js
@@ -17,51 +17,51 @@ function fetchData(query_type, fromYear, toYear, bbox) {
 				break;
 			case 'waterways':
 				//return nepalStats.all[1];
-				loadingDataDeffered[1].resolve(nepalStats[1], query_type);
+				loadingDataDeffered[1].resolve(nepalStats.all[1], query_type);
 				break;
 			case 'edu_institute':
 				//return nepalStats.all[2];
-				loadingDataDeffered[2].resolve(nepalStats[2], query_type);
+				loadingDataDeffered[2].resolve(nepalStats.all[2], query_type);
 				break;
 			case 'buildings':
 				//return nepalStats.all[3];
-				loadingDataDeffered[3].resolve(nepalStats[3], query_type);
+				loadingDataDeffered[3].resolve(nepalStats.all[3], query_type);
 				break;
 			case 'medical':
 				//return nepalStats.all[4];
-				loadingDataDeffered[4].resolve(nepalStats[4], query_type);
+				loadingDataDeffered[4].resolve(nepalStats.all[4], query_type);
 				break;
 			case 'financial_institute':
 				//return nepalStats.all[5];
-				loadingDataDeffered[5].resolve(nepalStats[5], query_type);
+				loadingDataDeffered[5].resolve(nepalStats.all[5], query_type);
 				break;
 			case 'gov_offices':
 				//return nepalStats.all[6];
-				loadingDataDeffered[6].resolve(nepalStats[6], query_type);
+				loadingDataDeffered[6].resolve(nepalStats.all[6], query_type);
 				break;
 			case 'historic_sites':
 				//return nepalStats.all[7];
-				loadingDataDeffered[7].resolve(nepalStats[7], query_type);
+				loadingDataDeffered[7].resolve(nepalStats.all[7], query_type);
 				break;
 			case 'natural_heritage':
 				//return nepalStats.all[8];
-				loadingDataDeffered[8].resolve(nepalStats[8], query_type);
+				loadingDataDeffered[8].resolve(nepalStats.all[8], query_type);
 				break;
 			case 'tourist_interest':
 				//return nepalStats.all[9];
-				loadingDataDeffered[9].resolve(nepalStats[9], query_type);
+				loadingDataDeffered[9].resolve(nepalStats.all[9], query_type);
 				break;
 			case 'settlement':
 				//return nepalStats.all[10];
-				loadingDataDeffered[10].resolve(nepalStats[10], query_type);
+				loadingDataDeffered[10].resolve(nepalStats.all[10], query_type);
 				break;
 			case 'e_i_y':
 				//return nepalStats.all[11];
-				loadingDataDeffered[11].resolve(nepalStats[11], query_type);
+				loadingDataDeffered[11].resolve(nepalStats.all[11], query_type);
 				break;
 			case 'users':
 				//return nepalStats.all[12];
-				loadingDataDeffered[12].resolve(nepalStats[12], query_type);
+				loadingDataDeffered[12].resolve(nepalStats.all[12], query_type);
 				break;
 		}
 	} else {
