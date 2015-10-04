be37f0dadf3b5c3b378234de2c76a5f2b4ea1144
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-10-04 09:11:07 +0545

    Update dataFeatcher3.js

--- src/dataFeatcher3.js
+++ src/dataFeatcher3.js
@@ -55,6 +55,7 @@ function fetchData(query_type, fromYear, toYear, bbox) {
 						var result = false;
 						for(var i = 0; i < years.length; i++)
 							result  = item.year.indexOf(years[i].toString()) !== -1 || result;
+						console.log(result);
 						return result;
 					})();
 					console.log('mybool = ' + mybool);
